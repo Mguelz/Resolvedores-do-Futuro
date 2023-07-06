e9e43fc1f86abd8b771e138d44dee716394f1d52
logonrmlocal <logonrmlocal@PA603MICRO01.fiap.com.br>
2023-07-06 19:36:37 -0300

    selectAll nas classes ApoliceDAO e CorretorDAO

--- src/br/com/fiap/repository/CorretorDAO.java
+++ src/br/com/fiap/repository/CorretorDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.fiap.connection.ConnectionFactory;
 import br.com.fiap.model.Corretor;
@@ -90,5 +92,26 @@ public class CorretorDAO {
 			e.printStackTrace();
 		}
 
+	}
+	
+	public List<Corretor> selectAll() {
+		List<Corretor> corretores = new ArrayList<Corretor>();
+		String sql = "select * from tb_sgr_corretor order by id_corretor";
+		try {
+			PreparedStatement stmt = conexao.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				Corretor corretor = new Corretor();
+				corretor.setIdCorretor(rs.getLong("id_corretor"));
+				corretor.setNome(rs.getString("ds_nome"));
+				corretor.setEndereco(rs.getString("ds_endereco"));
+			}
+			rs.close();
+			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return corretores;
 	}
 }//TODO FAZER SELECT ALL
